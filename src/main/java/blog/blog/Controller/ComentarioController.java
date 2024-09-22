f81bb66e71ed7182dbb0fddc57d840b34217da53
cristian mosquera trullo <cristiantrullo41@gmail.com>
2024-09-21 19:52:39 -0500

    se corrigieron errores gramaticales

--- src/main/java/blog/blog/Controller/ComentarioController.java
+++ src/main/java/blog/blog/Controller/ComentarioController.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,11 +12,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import blog.blog.Model.DTOs.ComentarioDTO;
 import blog.blog.Service.Impl.ComentarioServiceImpl;
 
-@Controller
+@RestController
 @RequestMapping("/Blog/Post/Comentarios")
 public class ComentarioController {
   
--- src/main/java/blog/blog/Controller/UsuarioController.java
+++ src/main/java/blog/blog/Controller/UsuarioController.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import blog.blog.Model.DTOs.UsuarioDTO;
 import blog.blog.Service.Impl.UsuarioServiceImpl;
 
-@Controller
+@RestController
 @RequestMapping("/Blog/Post/Usuario")
 public class UsuarioController {
   
